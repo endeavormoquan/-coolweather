0429cb4850e034ec340704ef9907d27c3f57f84f
ColonyAlbert <colonyalbert@gmail.com>
2017-07-06 23:43:42 +0800

    加入数据库配置

---
+++ app/src/main/java/com/example/colonyalbert/coolweather/db/County.java
@@ -0,0 +1,52 @@
+package com.example.colonyalbert.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by ColonyAlbert on 2017/7/6.
+ * Work Hard In Silence.
+ * Let Success Be Your Noise.
+ */
+
+public class County extends DataSupport {
+
+    private int id;
+
+    private String countyName;
+
+    private String weatherId;
+
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
